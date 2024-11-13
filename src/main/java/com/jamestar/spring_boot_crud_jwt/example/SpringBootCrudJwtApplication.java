30bc1ddf845a9d2709fbb20e56da6a939851fc73
Jamestar Kurbah <jamestarkurbah@gmail.com>
2024-11-13 23:18:47 +0530

    Intialization

---
+++ src/main/java/com/jamestar/spring_boot_crud_jwt/example/SpringBootCrudJwtApplication.java
@@ -0,0 +1,13 @@
+package com.jamestar.spring_boot_crud_jwt.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamestar/spring_boot_crud_jwt/example/SpringBootCrudJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamestar.spring_boot_crud_jwt.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
